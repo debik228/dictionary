5cb7fd429a40200e765dd155a5dde13b503fa4f7
Yevgen <oooooo.moja.oborona@gmail.com>
2020-10-26 16:59:12 +0200

    20-10-2020last : refactored main(String[])

--- Dictionary/Program.java
+++ Dictionary/Program.java
@@ -7,14 +7,6 @@ import java.sql.Connection;
 
 public class Program {
     public static void main(String[] args) throws Exception{
-
-        //for dbg
-        //Connection conn = null;
-        //try{
-        //    Class.forName("org.postgresql.Driver").getConstructor().newInstance();
-        //    conn = DriverManager.getConnection("jdbc:postgresql:test", "postgres", "123456789");
-        //}catch (Exception e){System.err.println(e);}
-
         Connection conn = null;
         try {
             conn = Common.getConn();
@@ -30,15 +22,15 @@ public class Program {
                 System.out.println("\nSelect what do you want to do:");
                 var mainMenuSelections = MainMenuSelections.class.getEnumConstants();
                 for (int i = 1; i <= mainMenuSelections.length; i++)
-                    System.out.println(i + ". " + mainMenuSelections[i - 1].name());
+                    System.out.printf("%d. %s", i ,mainMenuSelections[i - 1].name);
 
-                int selection = in.readLine().charAt(0);
-                selection -= 49;
-                if (selection > -1 && selection <= mainMenuSelections.length) {
+                int selection = -49 + in.readLine().charAt(0);
+                if (selection > -1 && selection <= mainMenuSelections.length)
                     mainMenuSelections[selection].action(stat);
-                }
             } while (!conn.isClosed());
-        }catch (Exception e){
+        }
+
+        catch (Exception e){
             e.printStackTrace();
             if(!conn.isClosed())conn.close();
             System.out.println("Press any key");
